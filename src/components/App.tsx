b9c32ed7d8161ff3667612f6f6fef3ee7d55a766
allivan2020 <allivan2020@gmail.com>
2026-02-25 20:14:26 +0200

    make firs and second part

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import css from './App.module.css';
+import CafeInfo from './CafeInfo';
+
+function App() {
+  return (
+    <>
+      <div className={css.app}>
+        <CafeInfo />{' '}
+      </div>
+    </>
+  );
+}
+
+export default App;
